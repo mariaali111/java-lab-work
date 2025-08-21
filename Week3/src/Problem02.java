c0dfe52d72a22c6badb2f982c2ddb8770c21ec88
trynnafindmaria <mariaali171104@gmail.com>
2025-08-21 20:46:22 +0530

    Added week 3 lab work

---
+++ Week3/src/Problem02.java
@@ -0,0 +1,8 @@
+public class Problem02 { // multiply two floating numbers
+    public static void main(String[] args) {
+        float x = 21.3f;
+        float y = 3.4f;
+        float product = x * y;
+        System.out.println(x + " x " + y + " = " + product);
+    }
+}
---
+++ Week3/src/Problem03.java
@@ -0,0 +1,7 @@
+public class Problem03 { // cube of a number
+    public static void main(String[] args) {
+        int a = 4;
+        double cube = Math.pow(a, 3);
+        System.out.println("Cube of " + a + " = " + cube);
+    }
+}
