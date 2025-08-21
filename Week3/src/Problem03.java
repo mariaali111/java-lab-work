f781478073454fe95d65b3468610392b9c52066b
trynnafindmaria <mariaali171104@gmail.com>
2025-08-21 20:53:37 +0530

    Delete Week3/src directory

--- Week3/src/Problem03.java
+++
@@ -1,7 +0,0 @@
-public class Problem03 { // cube of a number
-    public static void main(String[] args) {
-        int a = 4;
-        double cube = Math.pow(a, 3);
-        System.out.println("Cube of " + a + " = " + cube);
-    }
-}
