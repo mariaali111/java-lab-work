7ce83b1e4e47d912a6d9a0454c670a7c28e548c1
Maria Ali <mariaali171104@gmail.com>
2025-09-21 20:35:10 +0530

    Added README for week 6

--- Week 6/Problem02.java
+++ Week 6/Problem02.java
@@ -7,7 +7,12 @@ public class Problem02 { // prime
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
         boolean isPrime = true;
-
+        
+        if (n == 0 || n == 1) {
+            System.out.println(n + " is neither prime nor composite.");
+            return;
+        }
+        
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0){
                 isPrime = false;
@@ -19,3 +24,4 @@ public class Problem02 { // prime
         else System.out.println(n + " is not prime.");
     }
 }
+
